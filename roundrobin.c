c1e625e838d1769ff67ce83a184df7fb9a76e8b4
mlrit <pranavakrishna77@gmail.com>
2024-07-22 22:44:58 +0530

    roundrobin.c

--- roundrobin.c
+++ roundrobin.c
@@ -3,28 +3,23 @@
 int main() {
     int n, i, qt, count = 0, temp, sq = 0, bt[10], wt[10], tat[10], rem_bt[10];
     float wtavg = 0, tatavg = 0;
-    
-    // Get the number of processes
     printf("Enter the number of processes: ");
     scanf("%d", &n);
-    
-    // Get the burst time for each process
+
     for (i = 0; i < n; i++) {
         printf("Enter Burst Time for Process %d: ", i);
         scanf("%d", &bt[i]);
-        rem_bt[i] = bt[i]; // Initialize remaining burst time
+        rem_bt[i] = bt[i]; 
     }
-    
-    // Get the quantum time
+
     printf("Enter the quantum time: ");
     scanf("%d", &qt);
-    
-    // Execute processes in round robin order
+
     while (1) {
         int done = 1;
         for (i = 0; i < n; i++) {
             if (rem_bt[i] > 0) {
-                done = 0; // There is a pending process
+                done = 0; 
                 
                 if (rem_bt[i] > qt) {
                     rem_bt[i] -= qt;
@@ -37,24 +32,21 @@ int main() {
             }
         }
         
-        if (done == 1) // All processes are done
+        if (done == 1) 
             break;
     }
     
-    // Calculate turnaround time and waiting time
     for (i = 0; i < n; i++) {
         tat[i] = bt[i] + wt[i];
         wtavg += wt[i];
         tatavg += tat[i];
     }
     
-    // Display process details
     printf("\n\tPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\n");
     for (i = 0; i < n; i++) {
         printf("\tP%d\t\t%d\t\t%d\t\t%d\n", i, bt[i], wt[i], tat[i]);
     }
     
-    // Calculate and display average waiting and turnaround time
     printf("Average Waiting Time: %.2f\n", wtavg / n);
     printf("Average Turnaround Time: %.2f\n", tatavg / n);
     
