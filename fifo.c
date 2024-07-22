7a7168813a225c7b7f6216156021784ae21ee45f
mlrit <pranavakrishna77@gmail.com>
2024-07-22 20:03:32 +0530

    fifo.c

--- fifo.c
+++ fifo.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-
-int frames[3]; // Array to hold the pages in memory frames
-
-void display(); // Function prototype for displaying the current state of frames
-
+int frames[3]; 
+void display(); 
 int main() {
     int i, j;
-    int pages[7] = {7, 0, 1, 2, 0, 3, 0}; // Correct size of array for pages
+    int pages[7] = {7, 0, 1, 2, 0, 3, 0}; 
     int flag1, flag2, pageFaults = 0, frameSize = 3, top = 0;
-
-    // Initialize all frames to -1 indicating they are empty
     for(i = 0; i < frameSize; i++) {
         frames[i] = -1;
     }
-
-    // Iterate over each page to be loaded
     for(j = 0; j < 7; j++) {
         flag1 = 0;
         flag2 = 0;
-
-        // Check if the page is already in one of the frames
         for(i = 0; i < frameSize; i++) {
             if(frames[i] == pages[j]) {
                 flag1 = 1;
@@ -27,8 +18,6 @@ int main() {
                 break;
             }
         }
-
-        // If the page is not in any frame, try to place it in an empty frame
         if(flag1 == 0) {
             for(i = 0; i < frameSize; i++) {
                 if(frames[i] == -1) {
@@ -38,23 +27,16 @@ int main() {
                 }
             }
         }
-
-        // If the page is not in any frame and there are no empty frames, replace the oldest page
         if(flag2 == 0) {
             frames[top] = pages[j];
             top = (top + 1) % frameSize;
             pageFaults++;
         }
-
-        // Display the current state of frames
         display();
     }
-
     printf("Number of page faults: %d\n", pageFaults + frameSize);
-
     return 0;
 }
-
 void display() {
     int i;
     printf("\n");
