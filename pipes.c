ab02b87e97543d686cb3a8b8fb3e89aa4f4275da
mlrit <pranavakrishna77@gmail.com>
2024-07-22 21:10:03 +0530

    pipes.c

--- pipes.c
+++ pipes.c
@@ -30,3 +30,32 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <linux/stat.h>
+#define FIFO_FILE "w6.txt"
+int main(void) {
+    FILE *fp;
+    char readbuff[80];
+    umask(0);
+    mknod(FIFO_FILE, S_IFIFO | 0666, 0);
+    while (1) {
+        fp = fopen(FIFO_FILE, "r");
+        fgets(readbuff, 80, fp);
+        printf("Received string is: %s\n", readbuff);
+        fclose(fp);
+    }
+    exit(0);
+    return 0;
+}
+
