b39c6ffd351705b5f050ae5c380e4a3821554fa3
mlrit <pranavakrishna77@gmail.com>
2024-07-22 22:21:23 +0530

    message_queues.c

--- message_queues.c
+++ message_queues.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-
-// structure for message queue
 struct mesg_buffer {
     long msg_type;
     char msg_text[100];
 } message;
-
 int main() {
     key_t key;
     int msgid;
-
-    // ftok to generate unique key
     key = ftok("progfile", 65);
-
-    // msgget creates a message queue and returns identifier
     msgid = msgget(key, 0666 | IPC_CREAT);
-
     message.msg_type = 1;
-
     printf("Write Data: ");
-    gets(message.msg_text);
-
-    // msgsnd to send message
+    fgets(message.msg_text,10,stdin);
     msgsnd(msgid, &message, sizeof(message), 0);
-
-    // display the message
     printf("Data sent is: %s\n", message.msg_text);
-
     return 0;
 }
 
@@ -36,31 +22,17 @@ int main() {
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-
-// structure for message queue
 struct mesg_buffer {
     long msg_type;
     char msg_text[100];
 } message;
-
 int main() {
     key_t key;
     int msgid;
-
-    // ftok to generate unique key
     key = ftok("progfile", 65);
-
-    // msgget creates a message queue and returns identifier
     msgid = msgget(key, 0666 | IPC_CREAT);
-
-    // msgrcv to receive message
     msgrcv(msgid, &message, sizeof(message), 1, 0);
-
-    // display the message
     printf("Data received is: %s\n", message.msg_text);
-
-    // to destroy the message queue
     msgctl(msgid, IPC_RMID, NULL);
-
     return 0;
 }
